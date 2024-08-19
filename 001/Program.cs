ef62d85bab0603e8796246bf160c1796fafac01b
cl <1194096288@qq.com>
2024-08-19 23:30:20 +0800

    2329

---
+++ 001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World!");
+            Console.WriteLine("Hello,World!");
+            Console.WriteLine("Hello,World!");
+            Console.WriteLine("2024.0819.2329");
+        }
+    }
+}
